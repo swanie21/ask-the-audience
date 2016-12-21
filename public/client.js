0dabe6e6bee85cc0c561465686bc6a094b0e2e35
swanie21 <kswanie21@gmail.com>
2016-12-21 12:05:55 -0700

    deploy app

---
+++ public/client.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const socket = io();
+const connectionCount = document.getElementById('connection-count');
+const statusMessage = document.getElementById('status-message');
+const buttons = document.querySelectorAll('#choices button');
+const aTotal = document.querySelector('.a-total');
+const bTotal = document.querySelector('.b-total');
+const cTotal = document.querySelector('.c-total');
+const dTotal = document.querySelector('.d-total');
+const personalVote = document.getElementById('personal-vote');
+
+
+socket.on('usersConnected', function (count) {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+socket.on('statusMessage', function (message) {
+  statusMessage.innerText = message;
+});
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', this.innerText);
+  });
+}
+
+socket.on('voteCount', function (votes) {
+  aTotal.innerText = 'Total A Votes:' + votes.A;
+  bTotal.innerText = 'Total B Votes:' + votes.B;
+  cTotal.innerText = 'Total C Votes:' + votes.C;
+  dTotal.innerText = 'Total D Votes:' + votes.D;
+});
+
+socket.on('peronalVote', function (ownVote) {
+  personalVote.innerText = ownVote;
+});
---
+++ server.js
@@ -0,0 +1,60 @@
+'use strict';
+
+let votes = {};
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app)
+                 .listen(port, function () {
+                    console.log('Listening on port ' + port + '.');
+                  });
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+function countVotes(votes) {
+var voteCount = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0
+};
+  for (var vote in votes) {
+    voteCount[votes[vote]]++;
+  }
+  return voteCount;
+}
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
+
+  socket.emit('statusMessage', 'You have connected.');
+
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      socket.emit('peronalVote', message);
+      io.emit('voteCount', countVotes(votes));
+    }
+  });
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    socket.emit('voteCount', countVotes(votes));
+    io.sockets.emit('userConnection', io.engine.clientsCount);
+  });
+});
+
+module.exports = server;
